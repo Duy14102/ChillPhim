91179af2190733267fa08db8d59383086f401317
Duy14102 <bongdien77@gmail.com>
2024-10-21 00:08:45 +0700

    Fixing CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: "../.env" })
 mongoose.connect(process.env.REACT_APP_mongoAtlas).then(() => console.log('Connected To MongoDB')).catch((err) => { console.error(err); });
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://chill-phim.netlify.app");
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
